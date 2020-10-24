1402ab0eb86c57757ba4176ef0321a0cc5dfba9c
Prajwal <prajwal1234patil@gmail.com>
2020-10-24 19:55:26 +0530

    inital

---
+++ myscript.sh
@@ -0,0 +1,19 @@
+
+echo auto save
+#echo "prajwal is started autosave in  the vscode noice"
+#name="prajwal"
+#age="21"
+#learn="cybersecurity"
+#echo "$name is learning $learn is of age $age"
+
+read -p "enter ur name and age " name
+
+#echo "${name} is learning ${learn} is of age "
+if [ "$name"=="prajwal" ]
+then
+    echo $name
+    echo "this names are same"
+else
+    echo "this names are not same"
+
+fi
