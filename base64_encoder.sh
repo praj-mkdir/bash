cbf9bfb51e837485eb6c3eaf18231abdd36cf7d6
Prajwal <prajwal1234patil@gmail.com>
2020-11-26 19:32:23 +0530

    alternative way using for loop

--- base64_encoder.sh
+++ base64_encoder.sh
@@ -35,3 +35,31 @@ tomcat:password ----- dG9tY2F0OnBhc3N3b3JkCg==
 tomcat: ----- dG9tY2F0Ogo=
 tomcat:admin ----- dG9tY2F0OmFkbWluCg==
 tomcat:changethis ----- dG9tY2F0OmNoYW5nZXRoaXMK
+
+//alternative way using for loop 
+for text in $(cat tomcat.txt); do l=`echo $text `; k=`echo -n $text | base64 `; echo $l   -  $k ;   done                                                                                                    
+admin:password - YWRtaW46cGFzc3dvcmQ=
+admin: - YWRtaW46
+admin:Password1 - YWRtaW46UGFzc3dvcmQx
+admin:password1 - YWRtaW46cGFzc3dvcmQx
+admin:admin - YWRtaW46YWRtaW4=
+admin:tomcat - YWRtaW46dG9tY2F0
+both:tomcat - Ym90aDp0b21jYXQ=
+manager:manager - bWFuYWdlcjptYW5hZ2Vy
+role1:role1 - cm9sZTE6cm9sZTE=
+role1:tomcat - cm9sZTE6dG9tY2F0
+role:changethis - cm9sZTpjaGFuZ2V0aGlz
+root:Password1 - cm9vdDpQYXNzd29yZDE=
+root:changethis - cm9vdDpjaGFuZ2V0aGlz
+root:password - cm9vdDpwYXNzd29yZA==
+root:password1 - cm9vdDpwYXNzd29yZDE=
+root:r00t - cm9vdDpyMDB0
+root:root - cm9vdDpyb290
+root:toor - cm9vdDp0b29y
+tomcat:tomcat - dG9tY2F0OnRvbWNhdA==
+tomcat:s3cret - dG9tY2F0OnMzY3JldA==
+tomcat:password1 - dG9tY2F0OnBhc3N3b3JkMQ==
+tomcat:password - dG9tY2F0OnBhc3N3b3Jk
+tomcat: - dG9tY2F0Og==
+tomcat:admin - dG9tY2F0OmFkbWlu
+tomcat:changethis - dG9tY2F0OmNoYW5nZXRoaXM=
